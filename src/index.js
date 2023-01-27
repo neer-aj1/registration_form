234e8462a6a8d04b468b00bc621659447c7f2d7e
Neeraj Kumar <neerajkumar15439@gmail.com>
2023-01-27 13:48:16 +0530

    Updated index.html code

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ require("./Database/connection");
 //to select the port
 const port = process.env.PORT || 3000;
 
+//to find the path of the static files so that the can be read using app.use()
 const static_path = path.join(__dirname, "../public");
-
 app.use(express.static(static_path));
 
-
+//used to listen to the request made on the port
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
