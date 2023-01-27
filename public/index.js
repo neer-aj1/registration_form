29b8bc5ebb435a19fb56db68611a10ba5048fb0a
Neeraj Kumar <neerajkumar15439@gmail.com>
2023-01-27 15:19:07 +0530

    Added Form validation and modified index.html

--- public/index.js
+++ public/index.js
@@ -9,57 +9,81 @@ const confirmpass = document.getElementById('cpass');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    validateform();
+    validateForm();
 });
 
 const validateEmail = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
+    var atSymbol = email.indexOf("@");
+    if (atSymbol < 1) return false;
+    var dot = email.lastIndexOf(".");
+    if(dot <= atSymbol + 3) return false;
+    if(dot === email.length - 1) return false;
+    return true;
+}
 
 const setError= (input, err)=>{
     const formControl = input.parentElement;
-    const small = formControl.querySelector('small');
-    small.innerHTML = err;
+    const error = formControl.querySelector('.error');
+    error.innerHTML = err;
     formControl.classList.add('error');
     formControl.classList.remove('success');
 }
 
-const setSuccess = (element)=>{
+const setSuccess = element=>{
     const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('small');
+    const errorDisplay = inputControl.querySelector('.error');
     errorDisplay.innerText = '';
     inputControl.classList.add('success');
     inputControl.classList.remove('error');
 }
 
-const validateform = () => {
-    const fnameVal = fname.value.trim();
-    const lname = lname.value.trim();
-    const email = email.value.trim();
-    const phone = pnumber.value.trim();
-    const password = pass.value.trim();
-    const confirmpass = cpass.value.trim();
+const validateForm = () => {
+    const fnameval = fname.value.trim();
+    const lnameval = lname.value.trim();
+    const emailval = email.value.trim();
+    const phoneval = pnumber.value.trim();
+    const passwordval = pass.value.trim();
+    const confirmpassval = cpass.value.trim();
 
-    if(fnameVal===""){
-        setError(fnameVal, 'First Name is required');
+    if(fnameval===''){
+        setError(fname, 'First Name is required');
     }else{
-        setSuccess(fnameVal);
+        setSuccess(fname);
     }
-    if(lname===""){
+    if(lnameval===''){
         setError(lname, 'Last Name is required');
     }else{
         setSuccess(lname);
     }
-    if(email===""){
+
+    if(emailval===''){
         setError(email, 'Email is required');
-    }else if(!validateEmail(email)){
-        setError(email, 'Email is not valid');
     }
     else{
         setSuccess(email);
+    }
+
+    if(phoneval===''){
+        setError(pnumber, 'Phone Number is required');
+    }else if(phoneval.length!==10){
+        setError(pnumber, 'Phone Number must be of 10 digits');
+    }else{
+        setSuccess(pnumber);
+    }
+    if(passwordval===''){
+        setError(pass, 'Password is required');
+    }else if(passwordval.length <= 5){
+        setError(pass, 'Password must be at least 5 characters');
+    }
+    else{
+        setSuccess(pass);
+    }
+    if(confirmpassval===''){
+        setError(confirmpass, 'Confirm Password is required');
+    }else if(confirmpassval!=passwordval){
+        setError(confirmpass, 'Passwords do not match');
+    }
+    else{
+        setSuccess(confirmpass);
     }
 }
